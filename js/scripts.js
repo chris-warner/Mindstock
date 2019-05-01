c08e04c0ce3c733c65180d0c26a163832323228f
Puspa Sharma <blackice002@gmail.com>
2019-05-01 12:24:53 -0400

    login form sequelize update

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  "use strict";
+      var window_width 	 = $(window).width(),
+      window_height 		 = window.innerHeight,
+      header_height 		 = $(".default-header").height(),
+      header_height_static = $(".site-header.static").outerHeight(),
+      fitscreen 			 = window_height - header_height;
+     
+      $(".fullscreen").css("height", "70vh")
+      $(".fitscreen").css("height", fitscreen);
+  
+})
---
+++ server.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const passport = require('passport');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const env = require('dotenv').load();
+const exphbs = require('express-handlebars');
+
+// BodyParser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Passport
+app.use(
+  session({ secret: 'rHUyjs6RmVOD06OdOTsVAyUUCxVXaWci', resave: true, saveUninitialized: true })
+); // session secret
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Handlebars
+const viewsPath = path.join(__dirname, 'views');
+const layoutsPath = path.join(viewsPath, 'layouts');
+const partialsPath = path.join(viewsPath, 'partials');
+app.set('views', viewsPath);
+
+const exphbsConfig = exphbs.create({
+  defaultLayout: 'main',
+  layoutsDir: layoutsPath,
+  partialsDir: [partialsPath],
+  extname: '.hbs'
+});
+
+app.engine('hbs', exphbsConfig.engine);
+app.set('view engine', '.hbs');
+
+// Models
+const models = require('./models');
+
+// Express static assets
+app.use(express.static("public"));
+
+// Routes
+const htmlroutes = require('./routes/htmlroutes.js')(app, passport);
+
+// Load passport strategies
+require('./config/passport/passport.js')(passport, models.user);
+
+// Sync Database
+models.sequelize
+  .sync()
+  .then(function() {
+    console.log('Database Connected');
+
+    app.listen(3000, function(err) {
+      if (!err) console.log('Connected at http://localhost:3000');
+      else console.log(err);
+    });
+  })
+  .catch(function(err) {
+    console.log(err, 'Error on Database Sync. Please try again!');
+  });
