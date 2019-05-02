25289f48a1f49b946a7c0b8ab5fa165cd71a70e5
Christopher Warner <christopher.warner92@gmail.com>
2019-05-01 20:49:20 -0400

    rewrote the newticker to use ajax methods for getting data. Articles are retrieved and stored in the newsArray

--- app/public/assets/js/newsticker.js
+++ app/public/assets/js/newsticker.js
@@ -1,18 +1,31 @@
-const NewsAPI = require('newsapi');
-const newsapi = new NewsAPI('b9c6fbd474d04caa8fa8da391d95b607');
-// To query /v2/top-headlines
-// All options passed to topHeadlines are optional, but you need to include at least one of them
-newsapi.v2.topHeadlines({
-  q: 'finance',
-  category: 'business',
-  language: 'en',
-  country: 'us'
-}).then(response => {
-  console.log(response);
-  /*
-    {
-      status: "ok",
-      articles: [...]
+// const NewsAPI = require('newsapi');
+$( document ).ready( function () {
+  console.log( "newsticker.js ran" );
+  const apikey = "b9c6fbd474d04caa8fa8da391d95b607";
+  let newsFeed = [];
+  let newsUrl = "https://newsapi.org/v2/everything?q=finance&apiKey=" + apikey;
+  let newsArray = [];
+  // To query /v2/top-headlines
+  // All options passed to topHeadlines are optional, but you need to include at least one of them
+  var NewsArticle = function (author1,description1,title1,publishedAt1) {
+    this.author = author1,
+    this.description = description1,
+    this.title = title1,
+    this.publishedAt = publishedAt1;
     }
-  */
+  
+    $.get( newsUrl, function ( data ) {
+      for ( i in data ) {
+        for (var article in data[ i ]) {
+          var newsArticle = new NewsArticle(
+            data[ i ][article].author,
+            data[ i ][article].description,
+            data[ i ][article].title,
+            data[ i ][article].publishedAt);
+            newsArray.push(newsArticle);
+        }
+      }
+
+console.log(newsArray);
+});
 });
